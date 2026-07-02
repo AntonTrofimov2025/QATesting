ad97d5959699386cd49ca1ae3a1071a061318a31
AntonTrofimov2025 <xerurganton@gmail.com>
2026-07-02 23:53:20 +0200

    Finished this quite strange hw3, even though the last steps of it are complete insane because of broken callback button :D

---
+++ Homeworks/hw3/test_ich_hw3.py
@@ -0,0 +1,60 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+import pytest
+
+@pytest.fixture()
+def driver():
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.get("https://itcareerhub.de/ru")
+    yield driver
+    driver.quit()
+
+def test_is_display(driver):
+    ich_logo = driver.find_element(By.CSS_SELECTOR, '[alt="IT Career Hub"]')
+    assert ich_logo.is_displayed()
+
+    programs = driver.find_element(By.CSS_SELECTOR, '[href="#submenu:more"] span')
+    assert programs.is_displayed()
+
+    payments = driver.find_element(By.CSS_SELECTOR, '[href="#rec1921734713"] span')
+    assert payments.is_displayed()
+
+    about_us = driver.find_element(By.CSS_SELECTOR, '[href="#submenu:more2"] span')
+    assert about_us.is_displayed()
+    ActionChains(driver).move_to_element(about_us).perform()
+    contacts = driver.find_element(By.CSS_SELECTOR, '[href="/ru/contact-us"]')
+    assert contacts.is_displayed()
+
+    reviews = driver.find_element(By.CSS_SELECTOR, '[href="/reviews"] span')
+    assert reviews.is_displayed()
+
+    blog = driver.find_element(By.CSS_SELECTOR, '[href="https://blog.itcareerhub.de/"] span')
+    assert blog.is_displayed()
+
+    change_ru = driver.find_element(By.CSS_SELECTOR, '[href = "/ru"]')
+    assert change_ru.is_displayed()
+    change_de = driver.find_element(By.CSS_SELECTOR, '[href="/"]')
+    assert change_de.is_displayed()
+    ActionChains(driver).move_to_element(about_us).perform()
+    contacts.click()
+    sleep(3)
+    assert driver.current_url == 'https://itcareerhub.de/ru/contact-us'
+    driver.set_window_size(640, 720)
+
+    # callback = driver.find_element(By.XPATH, '//span[@class="tn-atom__button-text" and text()="ОБРАТНЫЙ ЗВОНОК"]')
+    callback = driver.find_element(By.CSS_SELECTOR, '[href="#popup:form-tr"] > div > span')
+    ActionChains(driver).scroll_by_amount(0, 450).perform()
+    sleep(1)
+    callback.click()
+    sleep(1)
+    # callback = driver.find_element(By.CSS_SELECTOR, 'a[href="#popup:form-tr"]')
+    # # sleep(1)
+    # callback.click()
+    # # ActionChains(driver).click(callback).perform()
+    # sleep(2)
+    assert "Запишитесь на " in driver.find_element(By.CSS_SELECTOR, 'div[field="tn_text_175871291756015470"]').text
